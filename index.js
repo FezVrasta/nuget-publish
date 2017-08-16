a5b4c5973f9d37979f52f7d753404fb946b28a40
Federico Zivolo <fzivolo@quid.com>
2017-08-16 17:05:02 +0200

    fix: Properly set  attribute on files list

--- index.js
+++ index.js
@@ -3,71 +3,70 @@
 const fs = require('fs');
 const path = require('path');
 const xml = require('xml');
-const ignoreToGlob = require('gitignore-to-glob');
-const glob = require('glob-fs')({ gitignore: false });
+const glob = require('glob-fs')({ gitignore: false, builtins: false });
+const mm = require('micromatch');
 const nuget = require('nuget-in-path');
 const pkg = require(path.resolve(process.cwd(), 'package.json'));
+const packlist = require('npm-packlist');
 
 const nuspecPath = path.resolve(process.cwd(), 'package.nuspec');
 
-// Convert npmignore to list of glob patterns
-const npmignore = ['!**/node_modules/**', '!**/package.nuspec']
-  .concat(ignoreToGlob(path.resolve(process.cwd(), '.npmignore')))
-  .filter(pattern => !(pattern.indexOf('/*/**') === pattern.length - 5));
+function getSourcePath(file) {
+  return path.dirname(`content/Scripts/${file.replace('dist/', '')}`);
+}
 
-// Use these patterns to match all the files that should get included
-// and generate a list of XML `file`
-const files = glob
-  .readdirSync(`(${npmignore.join('|')})`)
-  .map(
+packlist({ path: process.cwd() }).then(files => {
+  files = files.filter(file => file !== 'package.json').map(
     file =>
       fs.statSync(file).isFile() && {
         file: {
           _attr: {
             src: file,
+            source: getSourcePath(file),
           },
         },
       }
-  )
-  .filter(f => f);
+  );
 
-// Generate nuspec file
-const nuspec = xml(
-  [
-    {
-      package: [
-        {
-          _attr: {
-            xmlns: 'http://schemas.microsoft.com/packaging/2011/08/nuspec.xsd',
+  // Generate nuspec file
+  const nuspec = xml(
+    [
+      {
+        package: [
+          {
+            _attr: {
+              xmlns:
+                'http://schemas.microsoft.com/packaging/2011/08/nuspec.xsd',
+            },
           },
-        },
-        {
-          metadata: [
-            { id: pkg.name },
-            { version: pkg.version },
-            { description: pkg.description },
-            { authors: 'FezVrasta' },
-            { projectUrl: pkg.homepage },
-          ],
-        },
-        { files },
-      ],
-    },
-  ],
-  { declaration: true, indent: true }
-);
+          {
+            metadata: [
+              { id: pkg.name },
+              { version: pkg.version },
+              { description: pkg.description },
+              { authors: 'FezVrasta' },
+              { projectUrl: pkg.homepage },
+            ],
+          },
+          { files },
+        ],
+      },
+    ],
+    { declaration: true, indent: true }
+  );
 
-fs.writeFileSync(nuspecPath, nuspec);
+  fs.writeFileSync(nuspecPath, nuspec);
 
-nuget.pack(nuspecPath, (err, nupkg) => {
-  if (err) {
-    return console.error(err);
-  }
-  nuget.push(nupkg, err => {
-    fs.unlinkSync(nuspecPath);
+  nuget.pack(nuspecPath, (err, nupkg) => {
     if (err) {
       return console.error(err);
     }
-    console.info(`Nuget: Successfully pushed ${nupkg.path}`);
+    nuget.push(nupkg, err => {
+      fs.unlinkSync(nuspecPath);
+      if (err) {
+        return console.error(err);
+      }
+      console.info(`Nuget: Successfully pushed ${nupkg.path}`);
+    });
   });
 });
